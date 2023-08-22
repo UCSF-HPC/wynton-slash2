4a633e408665c317f530e79c69e5189220d574ef
hbslash <hbslash-github@h.braju.com>
2023-08-22 14:35:03 -0700

    Node status updates (qstat -f -qs uE)

--- cron-scripts/extract-total_time.sh
+++ cron-scripts/extract-total_time.sh
@@ -18,7 +18,7 @@ echo "Number of dirs: ${#dirs[@]}"
 for dir in "${dirs[@]}"; do
     drive="${dir//\//_}"
     src="${BENCH_LOGPATH}/bench-files-tarball_${drive}.tsv"
-    dst="${path}/wynton-bench_${drive}.tsv"
+    dst="${path}/wynton-bench_${host}_${drive}.tsv"
     echo "[INFO] Considering ${host}:${dir} == ${src}"
     if [[ -f "$src" ]]; then
         echo "[INFO] Processing ${src} -> ${dst}"
